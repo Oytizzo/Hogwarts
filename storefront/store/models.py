ef95e5d556978b6faad9df2cd99a7898f230fb86
oytizzo <oytizzo@gmail.com>
2023-09-13 16:10:57 +0600

    [update] customer 1toM order

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -43,6 +43,7 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(to=Customer, on_delete=models.SET_NULL)
 
 
 class Address(models.Model):
