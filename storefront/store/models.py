72ba080e94ced925fb027496d1ddaa39d1753cd3
oytizzo <oytizzo@gmail.com>
2023-09-13 16:01:14 +0600

    [update] address model to 1toM

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -44,4 +44,4 @@ class Order(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(to=Customer, on_delete=models.CASCADE, primary_key=True)
+    customer = models.ForeignKey(to=Customer, on_delete=models.CASCADE)
