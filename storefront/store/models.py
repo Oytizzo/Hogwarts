cbdc80cc277a0919beea987fa6cd774292ac638f
oytizzo <oytizzo@gmail.com>
2023-09-13 16:18:10 +0600

    [add] orderitem model with relations

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -43,7 +43,14 @@ class Order(models.Model):
     ]
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-    customer = models.ForeignKey(to=Customer, on_delete=models.SET_NULL)
+    customer = models.ForeignKey(to=Customer, on_delete=models.PROTECT)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(to=Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(to=Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Address(models.Model):
