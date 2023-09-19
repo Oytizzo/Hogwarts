3f9380493d1b8ad358ef3ab44b9b29aecaac0e49
oytizzo <oytizzo@gmail.com>
2023-09-20 00:10:25 +0600

    [update] changes for ORM

--- storefront/store/models.py
+++ storefront/store/models.py
@@ -16,8 +16,9 @@ class Collection(models.Model):
 class Product(models.Model):
     # sku = models.CharField(max_length=20, primary_key=True)
     title = models.CharField(max_length=255)    #varchar(255)
+    slug = models.SlugField()
     description = models.TextField()            #text
-    price = models.DecimalField(max_digits=6, decimal_places=2)     # 999999.99
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)     # 999999.99
     inventory = models.IntegerField()
     first_entry = models.DateTimeField(auto_now_add=True)   # auto_now_add updates one time when first product was added
     last_update = models.DateTimeField(auto_now=True)   # auto_now update everytime product updates
