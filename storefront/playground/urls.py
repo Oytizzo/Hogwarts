8f4d29bd9dd57ab12406d7e2c70a7a0af785db78
oytizzo <oytizzo@gmail.com>
2024-05-23 04:00:09 +0600

    Remove storefront

--- storefront/playground/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import log_hello
-
-urlpatterns = [
-    path("hello/", log_hello),
-]
--- storefront/playground/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from store.models import Product
-
-
-def log_hello(request):
-    # return HttpResponse("Hello")
-    query_set = Product.objects.all()
-    return render(request, "hello.html", {"products": query_set})
