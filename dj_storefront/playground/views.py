34d2365d786d4e734ffafe745dd8f5d791b80280
oytizzo <oytizzo@gmail.com>
2024-06-27 06:21:38 +0600

    [dj_storefront] add sample view

--- dj_storefront/playground/views.py
+++ dj_storefront/playground/views.py
@@ -1,3 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def hello_world(request):
+    x = 1
+    y = 2
+    sum = x + y
+    return HttpResponse("Hello World!")
